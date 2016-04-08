11864fb4cdcc93b58ccaca218dade68f4df9498e
alexmon1989 <alex.mon1989@gmail.com>
2016-04-08 09:32:30 +0300

    fix: copyright.

--- resources/views/layout/_partials/footer.blade.php
+++ resources/views/layout/_partials/footer.blade.php
@@ -3,7 +3,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <p class="text-center">&copy; ДП «Український інститут інтелектуальної власності», {{ date('Y') }} р.</p>
+                    <p class="text-center">&copy; ДП «Український інститут інтелектуальної власності», {{ date('Y') }}</p>
                 </div>
             </div>
         </div>
